757fa8fd63c705f1822294d6ca95d5f2912524a2
Mateja Dabovic <mi21271@alas.matf.bg.ac.rs>
2026-05-28 02:25:28 +0200

    Added configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+WINDOW_SIZE     = 512
+HOP_SIZE        = 256
+INPUT_DIM       = WINDOW_SIZE // 2 + 1
+
+HIDDEN_DIM      = 64
+
+NOIZEUS_DIR     = "noizeus"
+NOIZEUS_TOTAL   = 30
+
+MODEL_PATH      = "model_tezine.npz"
